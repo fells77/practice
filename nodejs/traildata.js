3a47297449c6d34c798730280e915a1188040df5
sfell77 <github@sfproductions.net>
2024-02-06 18:21:24 -0500

    ugh put json in right dir

--- nodejs/traildata.js
+++ nodejs/traildata.js
@@ -1,5 +1,5 @@
 // Require in trails module from trails.js
-const trails = require('../trails.json');
+const trails = require('./trails.json');
 // Require util module here!
 const util = require('util');
 
