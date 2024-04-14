08c7f0db17cc03ab6e94d56ff2744ea9c8b46778
sfell77 <github@sfproductions.net>
2024-04-13 20:54:53 -0400

    numpy test

---
+++ python/numtest.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def exclusion(arr1, arr2, arr3=None):
+  # arr1 = [1,2,3,4,5]
+  # arr2 = [1,2,5,5,5]
+  # arr3 = [2,2,3,4,5]
+
+  ara = np.array(arr1)
+  arb = np.array(arr2)
+  arc = np.array(arr3)
+
+  compare = ara == arb
+  print("compare:")
+  print(compare)
+
+  if arr3 is None:
+    result = ara[compare]
+
+  else:
+    exar = np.array([])
+    size = len(ara)
+    i = 0
+    while i < size:
+      if compare[i] == True:
+        exar = np.append(exar, ara[i])
+      else:
+        exar = np.append(exar, 'False')
+      i += 1
+      print("exar:")
+      print(exar)
+    
+    result = arc[exar == 'False']
+
+  return result
